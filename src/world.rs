ae8d6fbfab4362a5290904145d4d361e73f4c8bc
DeveloperGY <79323075+DeveloperGY@users.noreply.github.com>
2025-03-06 19:22:32 -0500

    Implemented the deletion of entities

--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,4 @@
-use crate::entity::EntityManager;
+use crate::entity::{Entity, EntityManager};
 
 pub struct World {
     entity_manager: EntityManager,
@@ -9,6 +9,17 @@ impl World {
         Self {
             entity_manager: EntityManager::new(max_entity_count),
         }
+    }
+
+    pub fn create_entity(&mut self) -> Option<Entity> {
+        self.entity_manager.create_entity()
+    }
+
+    pub fn delete_entity(&mut self, entity: Entity) {
+        // Safety: Due to the fact that entities cannot be cloned outside the
+        // crate and this function takes ownership of the entity handle, it is
+        // guaranteed that the entity was never deleted before.
+        unsafe { self.entity_manager.delete_entity(entity) };
     }
 }
 
